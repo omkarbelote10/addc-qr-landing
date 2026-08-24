c7bfe151aeba47b7771ee258a6ecb841ff0b0c2b
omkarbelote10 <omkarbelote10@gmail.com>
2026-08-25 03:16:40 +0530

    Added gps error simulation

--- src/drone_control_pkg/drone_control_pkg/mission_control_node.py
+++ src/drone_control_pkg/drone_control_pkg/mission_control_node.py
@@ -4,8 +4,12 @@ from mavros_msgs.msg import State
 from geometry_msgs.msg import PoseStamped, TwistStamped, Point
 from mavros_msgs.srv import CommandBool, SetMode, CommandTOL
 from enum import Enum
-import time
+import time, random, math
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
+from geographic_msgs.msg import GeoPoseStamped
+from sensor_msgs.msg import NavSatFix
+from pygeodesy.geoids import GeoidPGM
+
 
 class MissionPlannerNode(Node):
     def __init__(self):
@@ -19,6 +23,28 @@ class MissionPlannerNode(Node):
         self.target_last_seen = None
         self.target_available = False
         self.search_start_time = None
+        self.takeoff_height = 10.0
+        self.home_altitude = None
+
+        # GPS target Coordinates
+        self.declare_parameter('target_lat', 0.0)
+        self.declare_parameter('target_lon', 0.0)
+        self.declare_parameter('target_altitude', 0.0)
+        self.target_lat = self.get_parameter('target_lat').value
+        self.target_lon = self.get_parameter('target_lon').value
+        #will bw set at very first call back of the gps
+        self.target_alt = None
+        self.navigation_threshold = 0.5
+
+        # current coordinates
+        self.current_lat = None
+        self.current_lon = None
+        self.current_alt = None
+
+        # induce the error
+        self.get_logger().info(f"TARGET RECEIVED -> lat:{self.target_lat}, lon:{self.target_lon}")
+        self.target_lat, self.target_lon = self.induce_gps_error(self.target_lat, self.target_lon)
+        self.get_logger().info(f"Modified:{self.target_lat}, lon:{self.target_lon}, alt: {self.target_alt}")
 
         # Image
         self.image_center_x = 320
@@ -51,7 +77,11 @@ class MissionPlannerNode(Node):
         self.state_sub = self.create_subscription(State, "/mavros/state", self.state_cb, 10)
         self.pose_sub = self.create_subscription(PoseStamped, "/mavros/local_position/pose", self.pose_cb, qos)
         self.target_sub = self.create_subscription(Point, "/target_pixel", self.target_cb, 10)
+        self.global_position_sub = self.create_subscription(NavSatFix, '/mavros/global_position/global', self.gps_callback, qos)
+
         self.cmd_vel_pub = self.create_publisher(TwistStamped, "/mavros/setpoint_velocity/cmd_vel", 10)
+        self.global_setpoint_pub = self.create_publisher(GeoPoseStamped, '/mavros/setpoint_position/global', 10)
+
 
         # making few service clients
         self.arm_client = self.create_client(CommandBool, "/mavros/cmd/arming")
@@ -72,6 +102,18 @@ class MissionPlannerNode(Node):
 
     def pose_cb(self, msg):
         self.current_pose = msg
+
+    def gps_callback(self, msg):
+        self.current_lat = msg.latitude
+        self.current_lon = msg.longitude
+        self.current_alt = msg.altitude
+
+        if self.home_altitude is None:
+            self.home_altitude = msg.altitude
+            self.target_alt = self.elipseoid_to_amsl(self.target_lat, self.target_lon, self.home_altitude + self.takeoff_height)
+            self.get_logger().info(
+                f"Home altitude set: {self.home_altitude:.2f} m"
+            )
 
     def send_velocity(self, vx, vy, vz):
         msg = TwistStamped()
@@ -122,13 +164,54 @@ class MissionPlannerNode(Node):
                 return
             if not self.cmd_sent:
                 request = CommandTOL.Request()
-                request.altitude = 7.0
+                request.altitude = self.takeoff_height
                 future = self.takeoff_client.call_async(request)
                 self.cmd_sent = True
                 self.get_logger().info("takeogg cmd sent")
-            if self.current_pose.pose.position.z >= 6.8:
+            if self.current_pose.pose.position.z >= self.takeoff_height - 0.2:
                 self.get_logger().info("Takeoff completed")
                 self.cmd_sent = False
+                self.state = MissionState.NAVIGATE_TO_TARGET
+                self.get_logger().info("state changed to navigate to target")
+                # lock out the current orientation
+                self.target_orientation = self.current_pose.pose.orientation
+
+        elif self.state == MissionState.NAVIGATE_TO_TARGET:
+
+            if not self.current_state.connected:
+                self.get_logger().info("fcu is not connected")
+                return
+            if self.current_lat == None or self.current_lon == None:
+                self.get_logger().info("lat or lon is none")
+                return
+            msg = GeoPoseStamped()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.pose.position.latitude = self.target_lat
+            msg.pose.position.longitude = self.target_lon
+            msg.pose.position.altitude = self.target_alt
+            msg.pose.orientation = self.target_orientation
+
+            self.global_setpoint_pub.publish(msg)
+            self.get_logger().info(f"current altitude: {self.current_alt}")
+
+            meters_per_lat = 111320
+            meters_per_lon = (
+                111320 * math.cos(math.radians(self.target_lat))
+            )
+
+            north = (
+                self.target_lat - self.current_lat
+            ) * meters_per_lat
+
+            east = (
+                self.target_lon - self.current_lon
+            ) * meters_per_lon
+
+            distance = math.sqrt(north**2 + east**2)
+
+
+            if distance <= self.navigation_threshold:
+                print("switching to search target, distance:",)
                 self.state = MissionState.SEARCH_TARGET
 
         elif self.state == MissionState.SEARCH_TARGET:
@@ -254,17 +337,52 @@ class MissionPlannerNode(Node):
             self.kd * derivative
         )
         return output, integral
+    def induce_gps_error(self, latitude, longitude, max_error_m=4.0):
+
+        # Random error distance within the allowed radius
+        error_distance = max_error_m * math.sqrt(random.random())
+
+        # Random direction
+        theta = random.uniform(0, 2 * math.pi)
+
+        # Convert error into North/East displacement
+        north_error = error_distance * math.cos(theta)
+        east_error = error_distance * math.sin(theta)
+
+        # Approximate metres per degree
+        meters_per_degree_lat = 111_320
+
+        meters_per_degree_lon = 111_320 * math.cos(
+            math.radians(latitude)
+        )
+
+        # Convert metres to degrees
+        latitude_error = north_error / meters_per_degree_lat
+        longitude_error = east_error / meters_per_degree_lon
+
+        noisy_latitude = latitude + latitude_error
+        noisy_longitude = longitude + longitude_error
+
+        return noisy_latitude, noisy_longitude
+    @staticmethod
+    def elipseoid_to_amsl(lat, lon, elipsoid_alt):
+        geoid = GeoidPGM('/usr/share/GeographicLib/geoids/egm96-5.pgm', kind=-3)
+        N = geoid.height(lat, lon)
+        return elipsoid_alt - N
+
+
 
 class MissionState(Enum):
     WAIT_FOR_CONNECTIONM = 0
     SET_GUIDED_MODE = 1
     ARM = 2
     TAKEOFF = 3
-    SEARCH_TARGET = 4
-    ALIGN_TARGET = 5
-    DESCEND = 6
-    LAND = 7
-    FINISHED = 8
+    NAVIGATE_TO_TARGET = 4
+    SEARCH_TARGET = 5
+    ALIGN_TARGET = 6
+    DESCEND = 7
+    LAND = 8
+    FINISHED = 9
 
 
 def main(args = None):
--- src/drone_control_pkg/launch/landing.launch.py
+++ src/drone_control_pkg/launch/landing.launch.py
@@ -1,13 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
 
+    target_lat = LaunchConfiguration("target_lat")
+    target_lon = LaunchConfiguration("target_lon")
+
+    target_lat_arg = DeclareLaunchArgument('target_lat', default_value='-35.3632171', description="Target Latitude")
+    target_lon_arg = DeclareLaunchArgument('target_lon', default_value='149.1652704', description="Target Longitude")
+
+
+
     mission_control_node = Node(
         package='drone_control_pkg',
         executable='mission_control_node',
-        name='mission_control_node'
+        name='mission_control_node',
+        parameters=[
+            {
+                'target_lat': target_lat,
+                'target_lon': target_lon
+            }
+        ]
     )
 
     vision_tracker_node = Node(
@@ -18,6 +34,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        target_lat_arg,
+        target_lon_arg,
         mission_control_node,
         vision_tracker_node
     ])
